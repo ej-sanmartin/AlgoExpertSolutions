28b2928e5c4f035feda16cbe8b459441e5d80f9a
Edgar Jr. San Martin <edgar.sanmartinjr@gmail.com>
2020-07-12 04:52:10 -0400

    Create TwoNumberSum.cs

---
+++ TwoNumberSum.cs
@@ -0,0 +1,32 @@
+/*
+  Write a function that takes in a non-empty array of distinct integers an
+	integer representing a target sum. If any two numbers in the input array
+	sum up to the target sum, the function should return them in an array, in any order.
+	If no two numbers sum up to the target sum, the function should return an
+	empty array.
+	
+	Note that the target sum has to be obtained by summing two different integers
+	in the array; you can't add a single integer to itself in order to obtain
+	the target sum.
+	
+	You can assume that there will be at most one pair of numbers summing up to
+	the target sum.
+*/
+
+using System.Collections.Generic;
+
+public class Program {
+	public static int[] TwoNumberSum(int[] array, int targetSum) {
+		HashSet<int> recorder = new HashSet<int>();
+		foreach(int num in array){
+			int compliment = targetSum - num;
+			if(recorder.Contains(compliment)){
+				return new int[] { compliment, num };
+			} else {
+				recorder.Add(num);
+			}
+		}
+		
+		return new int[0];
+	}
+}
